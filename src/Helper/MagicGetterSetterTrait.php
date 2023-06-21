9310fbcc3fd7c089405886066438f8cb972ecc49
Vitor Mattos <vitor@php.rio>
2023-06-21 17:00:14 -0300

    Create invoice class and add extra items to "produção cooperativista" bill
    Signed-off-by: Vitor Mattos <vitor@php.rio>

---
+++ src/Helper/MagicGetterSetterTrait.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @copyright Copyright (c) 2023, Vitor Mattos <vitor@php.rio>
+ *
+ * @author Vitor Mattos <vitor@php.rio>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace ProducaoCooperativista\Helper;
+
+trait MagicGetterSetterTrait {
+	public function __call($name, $arguments) {
+		if (!preg_match('/^(?<type>get|set)(?<property>.+)/', $name, $matches)) {
+			throw new \LogicException(sprintf('Cannot set non existing property %s->%s = %s.', \get_class($this), $name, var_export($arguments, true)));
+		}
+		$property = lcfirst($matches['property']);
+		if (!property_exists($this, $property)) {
+			throw new \LogicException(sprintf('Cannot set non existing property %s->%s = %s.', \get_class($this), $name, var_export($arguments, true)));
+		}
+		if ($matches['type'] === 'get') {
+			return $this->$property;
+		}
+		$this->$property = $arguments[0] ?? null;
+		return $this;
+	}
+}
