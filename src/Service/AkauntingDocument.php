41aee53fd7d6ea017bfadd67f9f8edae0f734aaf
Vitor Mattos <vitor@php.rio>
2023-07-03 09:11:59 -0300

    Update taxes and items of FRRA bill
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/Service/AkauntingDocument.php
+++ src/Service/AkauntingDocument.php
@@ -502,13 +502,23 @@ class AkauntingDocument
 
     private function updateFrra(): self
     {
-        $total = array_reduce($this->items, function (float $total, array $item) {
-            if ($item['item_id'] === $this->itemsIds['frra']) {
+        // Sum all items that isn't taxes
+        $total = array_reduce($this->items, function (float $total, array $item)  {
+            $taxesIds = [
+                $this->itemsIds['INSS'],
+                $this->itemsIds['IRRF'],
+            ];
+            if (!in_array($item['item_id'], $taxesIds)) {
                 $total += $item['price'];
             }
             return $total;
         }, 0);
 
+        $cooperado = $this->getCooperado();
+        // Backup of current FRRA to don't lost this value after update "baseProducao"
+        $currentFrra = $cooperado->getFrra();
+
+        // Update the "baseProducao" with total of items that isn't tax
         $cooperado = $this->getCooperado();
         $cooperado->setIsFrra(true);
         $cooperado->setBaseProducao($total);
@@ -519,9 +529,10 @@ class AkauntingDocument
                 code: 'frra',
                 name: 'FRRA',
                 description: sprintf('Referente ao ano/mês: %s', $this->dates->getInicio()->format('Y-m')),
-                price: $cooperado->getBaseProducao()
+                price: $currentFrra
             )
             ->setTaxes()
+            ->setNote('Base de cálculo', $this->numberFormatter->format($cooperado->getBaseProducao()))
             ->save();
         return $this;
     }
@@ -547,6 +558,7 @@ class AkauntingDocument
             ->setCurrencyCode('BRL')
             ->setNote('Dia útil padrão de pagamento', sprintf('%sº', $this->dates->getPagamentoNoDiaUtil()))
             ->setNote('Previsão de pagamento no dia', $this->dates->getPrevisaoPagamentoFrra()->format('Y-m-d'))
+            ->setNote('Base de cálculo', $this->numberFormatter->format($cooperado->getBaseProducao()))
             ->setContactId($cooperado->getAkauntingContactId())
             ->setContactName($cooperado->getName())
             ->setContactTaxNumber($cooperado->getTaxNumber())
