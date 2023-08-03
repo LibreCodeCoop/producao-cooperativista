5ab5b7891e24b07a7ccc8b6172e602d40c559ab1
Vitor Mattos <vitor@php.rio>
2023-08-03 18:43:22 -0300

    Remoção de comentário desnecessário
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/Migration/Version20230803185730.php
+++ src/Migration/Version20230803185730.php
@@ -7,9 +7,6 @@ namespace ProducaoCooperativista\Migration;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20230803185730 extends AbstractMigration
 {
     public function getDescription(): string
