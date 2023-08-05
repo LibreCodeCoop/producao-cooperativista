34f272aa60fcc62654bc1f27721abdf0c53c4e1a
Vitor Mattos <vitor@php.rio>
2023-08-05 18:19:12 -0300

    Adição de lógica de arredondamento
    Valores não são salvos com mais de 2 casas decimais no banco
    
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/Service/Akaunting/Document/ADocument.php
+++ src/Service/Akaunting/Document/ADocument.php
@@ -165,6 +165,8 @@ abstract class ADocument
             return $this;
         }
         $item['total'] = ($total > 0 ? $total : $item['price']) * $item['quantity'];
+        $item['price'] = round($item['price']);
+        $item['total'] = round($item['total']);
         $item['discount'] = $discount;
         $item['order'] = $order;
         $found = array_filter($this->items, function (array $i) use ($item): bool {
