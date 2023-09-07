e87ce9e51d2731e4d5ece59a23e777b40dbc4be3
Vitor Mattos <vitor@php.rio>
2023-09-07 16:51:51 -0300

    Move main code to bootstrap file to be possible used by PHPUnit
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -23,6 +23,28 @@
 
 declare(strict_types=1);
 
+use Doctrine\Migrations\Tools\Console\ConsoleRunner as DoctrineMigrationsConsoleRunner;
+use Doctrine\ORM\Tools\Console\ConsoleRunner as DoctrineOrmConsoleRunner;
+use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use ProducaoCooperativista\Command\GetCategoriesCommand;
+use ProducaoCooperativista\Command\GetCustomersCommand;
+use ProducaoCooperativista\Command\GetInvoicesCommand;
+use ProducaoCooperativista\Command\GetNfseCommand;
+use ProducaoCooperativista\Command\GetProjectsCommand;
+use ProducaoCooperativista\Command\GetTaxesCommand;
+use ProducaoCooperativista\Command\GetTimesheetsCommand;
+use ProducaoCooperativista\Command\GetTransactionsCommand;
+use ProducaoCooperativista\Command\GetUsersCommand;
+use ProducaoCooperativista\Command\MakeProducaoCommand;
+use ProducaoCooperativista\DB\Database;
+use ProducaoCooperativista\Helper\Dates;
+use ProducaoCooperativista\Service\Source\Nfse;
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Application;
+
 $includeIfExists = function (string $file): bool {
     if (file_exists($file)) {
         include $file;
@@ -40,4 +62,53 @@ if ((!$loader = $includeIfExists(__DIR__ . '/../vendor/autoload.php')) && (!$loa
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->safeLoad();
 
-return $loader;
+error_reporting(-1);
+
+$containerBuilder = new \DI\ContainerBuilder();
+$containerBuilder->addDefinitions([
+    Logger::class => \DI\autowire()
+        ->constructor('PRODUCAO_COOPERATIVISTA')
+        ->method('pushHandler', new StreamHandler('logs/system.log')),
+    LoggerInterface::class => \DI\get(Logger::class),
+    Database::class => \DI\autowire(),
+    Dates::class => \DI\autowire()
+        ->constructorParameter('locationHolydays', \DI\env('HOLYDAYS_LIST', 'br-national')),
+    'ProducaoCooperativista\Service\Kimai\Source\*' => \DI\autowire('ProducaoCooperativista\Service\Kimai\Source\*'),
+    'ProducaoCooperativista\Provider\Akaunting\*' => \DI\autowire('ProducaoCooperativista\Provider\Akaunting\*'),
+    'ProducaoCooperativista\Service\Akaunting\Source\*' => \DI\autowire('ProducaoCooperativista\Service\Akaunting\Source\*'),
+    Nfse::class => \DI\autowire(),
+    'ProducaoCooperativista\Command\*Command' => \DI\autowire('ProducaoCooperativista\Command\*Command'),
+    NumberFormatter::class => \DI\autowire()
+        ->constructor(
+            \DI\env('LOCALE') ?? 'pt_BR',
+            NumberFormatter::CURRENCY,
+        ),
+    SingleManagerProvider::class => DI\factory(function (ContainerInterface $c) {
+        /** @var Database */
+        $database = $c->get(Database::class);
+        return new SingleManagerProvider($database->getEntityManager());
+    }),
+]);
+$container = $containerBuilder->build();
+
+$application = new Application();
+
+$application->addCommands([
+    $container->get(GetCustomersCommand::class),
+    $container->get(GetInvoicesCommand::class),
+    $container->get(GetNfseCommand::class),
+    $container->get(GetProjectsCommand::class),
+    $container->get(GetTimesheetsCommand::class),
+    $container->get(GetTransactionsCommand::class),
+    $container->get(GetCategoriesCommand::class),
+    $container->get(GetTaxesCommand::class),
+    $container->get(GetUsersCommand::class),
+    $container->get(MakeProducaoCommand::class),
+]);
+
+// Doctrine ORM
+DoctrineOrmConsoleRunner::addCommands($application, $container->get(SingleManagerProvider::class));
+
+// Doctrine Migrations
+$dependencyFactory = DoctrineMigrationsConsoleRunner::findDependencyFactory();
+DoctrineMigrationsConsoleRunner::addCommands($application, $dependencyFactory);
