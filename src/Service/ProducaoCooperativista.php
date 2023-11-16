fca65c75dd78b7c6b74a2c123cd22bc333bb0caf
Vitor Mattos <vitor@php.rio>
2023-11-16 03:17:20 -0300

    Adição de método que retorna dados para o relatório
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/Service/ProducaoCooperativista.php
+++ src/Service/ProducaoCooperativista.php
@@ -32,6 +32,7 @@ use Doctrine\DBAL\Query\QueryBuilder;
 use Exception;
 use NumberFormatter;
 use ProducaoCooperativista\DB\Database;
+use ProducaoCooperativista\Helper\Colors;
 use ProducaoCooperativista\Helper\Dates;
 use ProducaoCooperativista\Provider\Akaunting\Request;
 use ProducaoCooperativista\Service\Akaunting\Document\Taxes\Cofins;
@@ -791,6 +792,129 @@ class ProducaoCooperativista
         $result = $qb->executeQuery();
         $total = $result->fetchOne();
         return (float) $total;
+    }
+
+    public function exportData(): array
+    {
+        $this->getProducaoCooperativista();
+        $return = [
+            'percentual_dispendios' => [
+                'valor' => $this->percentualDispendios(),
+                'formula' => <<<FORMULA
+                    <pre>
+                    SE ({base_calculo_dispendios} > 0) &lbrace;
+                        {percentual_dispendios} = {taxa_administrativa} / {base_calculo_dispendios} * 100
+                    &rbrace; SENÃO &lbrace;
+                        {percentual_dispendios} = 0
+                    &rbrace;
+                    </pre>
+                    FORMULA
+            ],
+            'taxa_minima' => [
+                'valor' => $this->taxaMinima,
+                'formula' => '{taxa_minima} = {total_dispendios_internos}'
+            ],
+            'total_dispendios_internos' => [
+                'valor' => $this->getTotalDispendiosInternos(),
+                'formula' => '{total_dispendios_internos} = somatório de itens com categoria de dispêndio interno'
+            ],
+            'taxa_maxima' => [
+                'valor' => $this->taxaMaxima,
+                'formula' => '{taxa_minima} * 2 = {taxa_maxima}'
+            ],
+            'percentual_naximo' => ['valor' => $this->percentualMaximo],
+            'taxa_administrativa' => [
+                'valor' => $this->taxaAdministrativa,
+                'formula' => <<<FORMULA
+                    <pre>
+                    SE ({base_calculo_dispendios} * {percentual_naximo} / 100 >= {taxa_maxima}) &lbrace;
+                        {taxa_administrativa} = {taxa_maxima}
+                    &rbrace; SENÃO SE ({base_calculo_dispendios} * {percentual_naximo} / 100 >= {taxa_maxima}) &lbrace;
+                        {taxa_administrativa} = {base_calculo_dispendios} * {percentual_naximo} / 100
+                    &rbrace; SENÃO &lbrace;
+                        {taxa_administrativa} = {taxa_minima}
+                    &rbrace;
+                    </pre>
+                    FORMULA
+            ],
+            'base_calculo_dispendios' => [
+                'valor' => $this->getBaseCalculoDispendios(),
+                'formula' => '{base_calculo_dispendios} = {total_notas_clientes} - {total_dispendios_clientes}'
+            ],
+            'total_notas_clientes' => [
+                'valor' => $this->getTotalNotasClientes(),
+                'formula' => '{total_notas_clientes} = ' . implode(' + ', array_column($this->getEntradasClientes(), 'amount'))
+            ],
+            'total_dispendios_clientes' => [
+                'valor' => $this->getTotalDispendiosClientes(),
+                'formula' => '{total_dispendios_clientes} = ' . implode(' + ', array_column($this->getCustosPorCliente(), 'amount'))
+            ],
+            'total_sobras_distribuidas' => ['valor' => $this->getTotalSobrasDistribuidasNoMes()],
+            'total_sobras_do_mes' => [
+                'valor' => $this->getTotalSobrasDoMes(),
+                'formula' => '{total_sobras_do_mes} = {base_calculo_dispendios} + {total_sobras_distribuidas} - {total_dispendios_internos} - {base_producao}'
+            ],
+            'base_producao' => ['valor' => $this->getTotalBaseProducao()],
+            'percentual_librecode' => [
+                'valor' => $this->percentualLibreCode(),
+                'formula' => '{percentual_librecode} = {total_horas_librecode} * 100 / {total_horas_possiveis}',
+            ],
+            'total_horas_possiveis' => [
+                'valor' => $this->getTotalHorasPossiveis(),
+                'formula' => '{total_horas_possiveis} = {total_cooperados} * 8 * {dias_uteis_no_mes}'
+            ],
+            'total_cooperados' => ['valor' => $this->getTotalCooperados()],
+            'dias_uteis_no_mes' => ['valor' => $this->dates->getDiasUteisNoMes()],
+            'total_horas_librecode' => [
+                'valor' => $this->getTotalHorasLibreCode(),
+                'formula' => '{total_horas_librecode} = {total_segundos_librecode} / 60 / 60'
+            ],
+            'total_segundos_librecode' => ['valor' => $this->getTotalSegundosLibreCode()],
+            'ano_mes' => ['valor' => $this->dates->getInicio()->format('Y-m')],
+        ];
+        return $this->formatData($return);
+    }
+
+    private function listToNumber(array $list, int $max): array
+    {
+        $total = count($list);
+        $return = [];
+        $distance = floor($max / $total);
+        $current = 0;
+        while(count($return) < $total) {
+            $current += $distance;
+            $return[] = $current;
+        }
+        return $return;
+    }
+
+    private function formatData(array $data): array
+    {
+        $positions = $this->listToNumber(array_keys($data), count(Colors::list));
+        $colorList = array_keys(Colors::list);
+        $sequence = 0;
+        foreach ($data as $key => $value) {
+            if (!empty($data[$key]['formula'])) {
+                preg_match_all('/\{(?<name>\w+)\}/', $value['formula'], $matches, PREG_SET_ORDER, 0);
+                foreach ($matches as $match) {
+                    $value['formula'] = preg_replace(
+                        '/\{' . $match['name'] . '\}/',
+                        '<span class="' . $match['name'] . '">' . $data[$match['name']]['valor'] . '</span>',
+                        $value['formula']
+                    );
+                }
+                $data[$key]['formula'] = preg_replace(['/{/', '/}/'], ['<span class="', '"></span>'], $value['formula']);
+            } else {
+                $data[$key]['formula'] = "<span class=\"$key\">{$data[$key]['valor']}</span>";
+            }
+            if (empty($value['color'])) {
+                $data[$key]['color'] = Colors::list[$colorList[$positions[$sequence]]];
+                $sequence++;
+            }
+            $data[$key]['font_color'] = Colors::colorContrast(...$data[$key]['color']);
+            $data[$key]['label'] = $value['label'] ?? ucfirst(str_replace('_', ' ', $key));
+        }
+        return $data;
     }
 
     public function exportToCsv(): string
