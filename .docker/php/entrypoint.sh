3939d410823984670956baeeabc9559485418582
Vitor Mattos <vitor@php.rio>
2023-12-10 22:42:49 -0300

    Implementação de nginx
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- .docker/php/entrypoint.sh
+++ .docker/php/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Set uid of host machine
+usermod --non-unique --uid "${HOST_UID}" www-data
+groupmod --non-unique --gid "${HOST_GID}" www-data
+
 # Wait for database
 php /var/www/scripts/wait-for-db.php
 
 # Start PHP-FPM
-php -S 0.0.0.0:80 -t /app/public/
+php-fpm
