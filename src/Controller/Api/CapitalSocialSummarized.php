94744edc8c379fbe9995cc997ac30a43d34af529
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-08 16:20:56 -0300

    fix(route): use attribute Route in CapitalSocialSummarized controller
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/Controller/Api/CapitalSocialSummarized.php
+++ src/Controller/Api/CapitalSocialSummarized.php
@@ -30,7 +30,7 @@ use App\Service\ProducaoCooperativista;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class CapitalSocialSummarized extends AbstractController
 {
