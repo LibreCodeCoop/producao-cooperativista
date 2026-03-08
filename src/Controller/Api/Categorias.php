cb5187cc54b43e985519df468e59a34fe32af597
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-08 16:20:55 -0300

    fix(route): use attribute Route in Categorias controller
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/Controller/Api/Categorias.php
+++ src/Controller/Api/Categorias.php
@@ -30,7 +30,7 @@ use App\Service\Akaunting\Source\Categories;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class Categorias extends AbstractController
 {
